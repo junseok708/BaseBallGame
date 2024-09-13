75c3a8cf8653826e821735ef1add5de49c49ecb4
junseok <junseok708@gmail.com>
2024-09-13 11:57:53 +0900

    [중간 저장] Game Class생성

---
+++ src/Game.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    Random rd = new Random();
+    Scanner sc = new Scanner(System.in);
+    String numCheck = "^[1-9]*$";
+    String correct = "";
+    int tryNum = 0;
+    int count = 0;
+    int random = 0;
+    int number = 3;
+
+
+
+    public String correct() {
+        while (true) {
+            count++;
+            random = (int) rd.nextInt(9) + 1;
+            if (correct.contains(Integer.toString(random))) {
+                count--;
+            } else {
+                correct += random;
+            }
+            if (count == number) {
+                break;
+            }
+        }
+        System.out.println(correct);
+        return correct;
+    }
+
+    public void strat(String correct) {
+        while (true) {
+            String answer = sc.nextLine();
+            if (!answer.equals(correct)) {
+                int strikeCount = 0;
+                int bollCount = 0;
+                System.out.println(answer);
+                for (int i = 0; i < answer.length(); i++) {
+                    for (int j = 0; j < correct.length(); j++) {
+                        if (correct.charAt(j) == answer.charAt(i)) {
+                            if (j == i) {
+                                strikeCount++;
+                            } else {
+                                bollCount++;
+                            }
+                        }
+                    }
+                }
+                if (strikeCount == 0 && bollCount == 0) {
+                    tryNum++;
+                    System.out.println("Out!!");
+                } else {
+                    tryNum++;
+                    System.out.println("스크라이크 : " + strikeCount);
+                    System.out.println("볼 : " + bollCount);
+                }
+            } else {
+                System.out.println("축하 합니다~");
+                System.out.println("승리 하셨습니다!!");
+                System.out.println("시도 횟수 : " + tryNum);
+                break;
+            }
+        }
+    }
+}
